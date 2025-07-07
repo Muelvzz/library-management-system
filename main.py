b25c3193b503e6e4bd9d401b2b52f641f02b737a
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-07-07 10:18:27 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+class Library():
+
+    def __init__(self):
+        pass
+
+    def add_books(self):
+        pass
+
+    def delete_books(self):
+        pass
+
+    def show(self):
+        pass
+
+class Frontend():
+    def __init__(self):
+        pass
+
+    def show_web(self):
+        st.title("Library Management System")
+
+if __name__ == "__main__":
+    frontend = Frontend()
+
+    frontend.show_web()
